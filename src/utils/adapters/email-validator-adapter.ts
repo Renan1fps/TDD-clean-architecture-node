98f7204891b80402c1c97f7850d4f2d57eac2212
Renan <renan.sergipe68@gmail.com>
2022-04-11 20:30:36 -0300

    refactor: rename email provider

--- src/utils/adapters/email-validator-adapter.ts
+++ src/utils/adapters/email-validator-adapter.ts
@@ -1,7 +1,7 @@
 import { IEmailValidator } from '../../presentation/protocols/email-validator'
 import validator from 'validator'
 
-export class EmailValidator implements IEmailValidator {
+export class EmailValidatorAdapter implements IEmailValidator {
   isValid (email: string): boolean {
     return validator.isEmail(email)
   }
