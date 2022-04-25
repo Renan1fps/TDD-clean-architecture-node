109b25c6a2a3975d3bc148cf234feab968451392
Renan <renan.sergipe68@gmail.com>
2022-04-24 22:57:19 -0300

    feat: add Singleton mongodb helper

---
+++ src/infra/db/mongo/account-repository/account.ts
@@ -0,0 +1,24 @@
+import { MongoClient } from 'mongodb'
+
+// Singleton
+export class MongoHelper {
+  private client: MongoClient = null
+  private static _instance: MongoHelper
+
+  private constructor () { }
+
+  static get intance (): MongoHelper {
+    if (!this._instance) {
+      MongoHelper._instance = new MongoHelper()
+    }
+    return this._instance
+  }
+
+  async connect (url: string): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL)
+  }
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  }
+}
