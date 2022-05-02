1f35348b36f4fa71dc5677309a8c174194051444
Renan <renan.sergipe68@gmail.com>
2022-05-01 22:03:31 -0300

    refactor: move sut to a factory helper method

--- src/infra/db/mongo/account-repository/account.spec.ts
+++ src/infra/db/mongo/account-repository/account.spec.ts
@@ -1,6 +1,10 @@
 import { MongoHelper } from '../helper/mongodeb-helper'
 import { AccountMongoRepository } from './account'
 
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
+
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.intance.connect(process.env.MONGO_URL)
@@ -11,7 +15,7 @@ describe('Account Mongo Repository', () => {
   })
 
   test('Should return account on success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
